8f7c61928208b662e1007d610143eb05ee95da4e
Sergey Pilovets <m-c@tut.by>
2017-12-06 10:50:55 +0300

    AddedGitTest class

---
+++ PVT.Money.GITTest/TestClass.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PVT.Money.GITTest
+{
+    class TestClass
+    {
+        private float testProperty;
+        public float TestProperty
+        {
+            get
+            {
+                return testProperty;
+            }
+            set
+            {
+                testProperty = value;
+            }
+        }
+
+        public void TestMethod( int a, string str)
+        {
+
+        }
+
+        public decimal AnyMethod()
+        {
+            return 10000000000000000000m;
+        }
+        
+    }
+}
