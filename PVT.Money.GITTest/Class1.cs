9c7a659d7f5c754c24fa8ef1153a8c3ba8341e87
Nadya Hadanovich <n.hadanovich@gmail.com>
2017-12-06 10:51:28 +0300

    Тест

---
+++ PVT.Money.GITTest/Class1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PVT.Money.GITTest
+{
+    class Class1
+    {
+        private float testProperty;
+
+        public float TestProperty
+        {
+            get { return testProperty; }
+            set { testProperty = value; }
+        }
+        public void TestMethod(int number, string text)
+        {
+            
+        }
+        public decimal Money ()
+        {
+            return 8;
+        }
+
+    }
+}
