13a4121e668fe8e907b796d530f6142dbca18d26
Александр Кравчук <xandraele@gmail.com>
2017-12-06 21:38:36 +0300

    Добавлен проект для бизнес-логики Добавлен проект для модульного тестирования

---
+++ PVT.Money.Business.Tests/UnitTest.cs
@@ -0,0 +1,17 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PVT.Money.Business.Tests
+{
+    [TestFixture]
+    class UnitTest
+    {
+        [Test]
+        public void SystemCheck()
+        {
+            Assert.Pass("Проверка системы тестирования");
+        }
+    }
+}
