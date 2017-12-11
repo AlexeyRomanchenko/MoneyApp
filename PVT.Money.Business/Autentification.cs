90ea210e09c1a8c9a721f6391432931c2069cb3e
Siarhei Izokh <sergey1001@tut.by>
2017-12-11 10:33:01 +0300

    Добален класс-заглушка User и класс Autentification

---
+++ PVT.Money.Business/Autentification.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PVT.Money.Business
+{
+    class Autentification
+    {
+
+        private string login;
+        private string password;
+        private User user;
+
+        public Autentification(string login, string password)
+        {
+            this.login = login;
+            this.password = password;
+        }
+
+        public User CheckAutentification()
+        {
+
+            User userResult = new User();
+            
+            foreach (User user in List<User>)
+            {
+                if (user.Login == this.login && user.Password == this.password)
+                {
+                    userResult = user;
+                }
+            }
+                        
+            return userResult;
+        }
+
+    }
+}
