5c3e40917873fe9bc2153e14ba5b916f13ef68d4
Dmitry Lahutski <Dzuke911@gmail.com>
2017-12-11 10:16:11 +0300

    Created AuthorisationTest Class

---
+++ PVT.Money.Business.Tests/AuthorisationTest.cs
@@ -0,0 +1,16 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PVT.Money.Business.Tests
+{
+    [TestFixture]
+    public class AuthorisationTest
+    {
+        public void AuthorisationOK()
+        {
+
+        }
+    }
+}
