01b9a6b0ab1912a552e6c0b8cea77fa600cb5a16
Anton Bogdan <annttonn_20@mail.ru>
2017-12-11 10:35:27 +0300

    Создана логика класса

---
+++ PVT.Money.Business/Registration.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PVT.Money.Business
+{
+    class Registration
+    {
+        string name;
+        string email;
+        string login;
+        string password1;
+        string password2;      
+
+        public Registration()
+        {
+            // Конструктор
+        }
+
+        public bool IsLoginCorrect()
+        {
+            // Логика метода
+            return true;
+        }
+
+        public bool IsPasswordCorrect()
+        {
+            // Логика метода
+            return true;
+        }
+
+        public bool IsPasswordCoinside()
+        {
+            // Логика метода
+            return true;
+        }
+        
+        public User UserCreate()
+        {
+            // Логика метода
+            return User;
+        }
+
+    }
+}
