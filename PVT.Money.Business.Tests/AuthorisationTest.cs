f6cbadbabf72ea494939b36f650026b7151f8ca6
Dmitry Lahutski <Dzuke911@gmail.com>
2017-12-11 10:55:36 +0300

    some test changes

--- PVT.Money.Business.Tests/AuthorisationTest.cs
+++ PVT.Money.Business.Tests/AuthorisationTest.cs
@@ -2,15 +2,27 @@
 using System;
 using System.Collections.Generic;
 using System.Text;
+using PVT.Money.Business;
 
 namespace PVT.Money.Business.Tests
 {
     [TestFixture]
     public class AuthorisationTest
     {
+        [Test]
         public void AuthorisationOK()
         {
+            string login = "tdfgdfg";
+            string password = "123abc123";
+            string name = "Vasya";
+            Enums.UserRoles uRoleAdmin = Enums.UserRoles.Admin;
 
+            Models.UserModel uModel = new Models.UserModel();
+
+            uModel.Login = login;
+            uModel.Password = password;
+            uModel.Name = name;
+            uModel.Role = uRoleAdmin;
         }
     }
 }
