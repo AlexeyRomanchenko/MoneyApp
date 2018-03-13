26f321b457c5966b7c6e3e57094104c608ef0e16
Alexey <romanchenko.alek@mail.ru>
2018-03-13 19:36:49 +0300

    commit

--- PVT.Money.Shell.Web/wwwroot/js/app.jsx
+++ PVT.Money.Shell.Web/wwwroot/js/app.jsx
@@ -1,9 +1,31 @@
-﻿class Hello extends React.Component {
-    render() {
-        return <h1>Привет</h1>;
+﻿function OneCurr()
+{
+    class Hello extends React.Component {
+        render() {
+            return <div>
+                <div className="col-md-6">
+                    <input className="form-control" type="text" placeholder="Введите сумму" />
+                </div>
+                <div className="col-md-6">
+                <div class="dropdown">
+                    <a class="dropdown-toggle" href="#" data-toggle="dropdown">
+                        <small class="text-muted">Выбрать кошелек <b class="caret"></b></small>
+                    </a>
+                    <ul class="dropdown-menu animated flipInX m-t-xs">
+                        <li><a href="#">На счет в этой валюте</a></li>
+                        <li><a href="#">На счет в другую валюту</a></li>
+                    </ul>
+                </div>
+                </div>
+                <br /><br />
+                <br />
+                <button className="btn btn-default">Перевести</button>
+                </div>;
+        }
     }
+    ReactDOM.render(
+        <Hello />,
+        document.getElementById("content")
+    )
+    
 }
-ReactDOM.render(
-    <Hello />,
-    document.getElementById("content")
-)
