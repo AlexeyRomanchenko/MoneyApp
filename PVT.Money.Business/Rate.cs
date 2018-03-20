8a37192e819fd4e6b8c2e46de53b2e64eb79453b
Alexey <romanchenko.alek@mail.ru>
2018-03-20 21:35:56 +0300

    old  GetRate method deleting

--- PVT.Money.Business/Rate.cs
+++ PVT.Money.Business/Rate.cs
@@ -11,70 +11,7 @@ namespace PVT.Money.Business
     {
 
         public decimal rateCount;
-        //Получаем коэффициент исходя ил двух наименований валют
-        //private void GetRate(Currency frst, Currency sec)
-        //{
-        //    decimal koef = -1;
-        //    if (frst == Currency.USD)
-        //    {
-        //        switch (sec)
-        //        {
-        //            case Currency.AUD:
-        //                {
-        //                    koef = 1.25m;
-        //                    break;
-        //                }
-        //            case Currency.EUR:
-        //                {
-        //                    koef = 0.81m;
-        //                    break;
-        //                }
-        //        }
-        //        rateCount = koef;
-        //        //return koef;
-        //    }
-        //    else if (frst == Currency.AUD)
-        //    {
-        //        switch (sec)
-        //        {
-        //            case Currency.USD:
-        //                {
-        //                    koef = 0.8m;
-        //                    break;
-        //                }
-        //            case Currency.EUR:
-        //                {
-        //                    koef = 0.65m;
-        //                    break;
-        //                }
-        //        }
-        //        rateCount = koef;
-        //        //return koef;
-        //    }
-        //    else if (frst == Currency.EUR)
-        //    {
-        //        switch (sec)
-        //        {
-        //            case Currency.AUD:
-        //                {
-        //                    koef = 1.53m;
-        //                    break;
-        //                }
-        //            case Currency.USD:
-        //                {
-        //                    koef = 1.23m;
-        //                    break;
-        //                }
-        //        }
-        //        rateCount = koef;
-        //        //return koef;
-        //    }
-        //    else
-        //    {
-        //        throw new Exception("We haven't such kind of money, sorry Man");
-        //    }
-        //}
-
+       
         private void GetRate(string first, string second)
         {
             using (HttpClient client = new HttpClient())
